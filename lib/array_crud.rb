604ff3adfe53fa59b4faea7f645a42bae9e1be50
Melissa Sperry <sperry.mel@gmail.com>
2020-02-15 04:46:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ end
 
 def remove_element_from_start_of_array(array)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-"wow" = array.shift
+wow = array.shift
 puts array.inspect
 puts "wow"
 end
